e778dd2c4a9c93970f136e76f1978b30346fe2be
AkhileshManda <manda.akhilesh4@gmail.com>
2022-10-18 17:33:21 +0530

    :tada: project init

---
+++ lib/auth/model/user.dart
@@ -0,0 +1,17 @@
+import 'dart:core';
+
+class User{
+  String? id;
+  String? email;
+  String? profilePicture;
+  String? description;
+  String? codeforcesUsername;
+  String? githubUsername;
+  String? leetcodeUsername;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+  List<String>? followedByIds;
+  List<String>? followingIds;
+  
+
+}
