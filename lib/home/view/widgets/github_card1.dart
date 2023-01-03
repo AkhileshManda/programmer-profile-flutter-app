4d1aa69079352b127fbda0b98036e69d0ec77fe1
AkhileshManda <manda.akhilesh4@gmail.com>
2023-01-03 23:25:49 +0530

    :lipstick: Updated github cards

--- lib/home/view/widgets/github_card1.dart
+++ lib/home/view/widgets/github_card1.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:glassmorphism/glassmorphism.dart';
 
 Widget githubCard1(
-    {
-    required String totalCommits,
+    {required String totalCommits,
     required String prs,
     required String issues,
-    required String contributedTo}){
-  return Container(
-    decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: const BorderRadius.all(Radius.circular(20)),
-        border: Border.all(color: Colors.grey)),
+    required String contributedTo}) {
+  return GlassmorphicContainer(
+    width: 350,
+    height: 200,
+    borderRadius: 20,
+    blur: 20,
+    alignment: Alignment.bottomCenter,
+    border: 2,
+    linearGradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          const Color(0xFFffffff).withOpacity(0.1),
+          const Color(0xFFFFFFFF).withOpacity(0.05),
+        ],
+        stops: const [
+          0.1,
+          1,
+        ]),
+    borderGradient: LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [
+        const Color(0xFFffffff).withOpacity(0.5),
+        const Color((0xFFFFFFFF)).withOpacity(0.5),
+      ],
+    ),
     child: Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(
@@ -18,10 +40,14 @@ Widget githubCard1(
         children: [
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text("Total Commits : $totalCommits"),
+                child: Text(
+                  "Total Commits : $totalCommits",
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
               ),
               // Padding(
               //   padding: const EdgeInsets.all(8.0),
@@ -29,23 +55,34 @@ Widget githubCard1(
               // ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text("Total PRs : $prs "),
+                child: Text(
+                  "Total PRs : $prs ",
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text("Total Issues : $issues"),
+                child: Text(
+                  "Total Issues : $issues",
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text("Contributed to: $contributedTo"),
+                child: Text(
+                  "Contributed to: $contributedTo",
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
               )
-
             ],
-              
           ),
           const Padding(
             padding: EdgeInsets.all(8.0),
-            child: Icon(Icons.person, size: 100),
+            child: FaIcon(
+              FontAwesomeIcons.github,
+              color: Colors.white,
+              size: 100,
+            ),
           )
         ],
       ),
--- lib/home/view/widgets/github_streak_card.dart
+++ lib/home/view/widgets/github_streak_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:glassmorphism/glassmorphism.dart';
 
 class GithubStreakGraph extends StatelessWidget {
   final String currentStreakLength;
@@ -6,56 +7,96 @@ class GithubStreakGraph extends StatelessWidget {
   final String currentStreakStartDate;
   final String longestStreakStartDate;
   final String longestStreakEndDate;
-  
-  const GithubStreakGraph({super.key, required this.currentStreakLength, required this.longestStreakLength, required this.currentStreakStartDate, required this.longestStreakStartDate, required this.longestStreakEndDate});
+
+  const GithubStreakGraph(
+      {super.key,
+      required this.currentStreakLength,
+      required this.longestStreakLength,
+      required this.currentStreakStartDate,
+      required this.longestStreakStartDate,
+      required this.longestStreakEndDate});
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-    decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: const BorderRadius.all(Radius.circular(20)),
-        border: Border.all(color: Colors.grey)),
-    child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("Current Streak Start Date ${currentStreakStartDate.substring(0,10)}"),
-              ),
-              // Padding(
-              //   padding: const EdgeInsets.all(8.0),
-              //   child: Text("Total Stars Earned : $totalStars"),
-              // ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("Current Streak Length $currentStreakLength"),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("Longest Streak Start Date ${longestStreakStartDate.substring(0,10)}"),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("Longest Streak End Date ${longestStreakEndDate.substring(0,10)}"),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("Longest Streak length $longestStreakLength"),
-              )
-
-            ],
-              
-          ),
-          
+    return GlassmorphicContainer(
+      width: 350,
+      height: 250,
+      borderRadius: 20,
+      blur: 20,
+      alignment: Alignment.bottomCenter,
+      border: 2,
+      linearGradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            const Color(0xFFffffff).withOpacity(0.1),
+            const Color(0xFFFFFFFF).withOpacity(0.05),
+          ],
+          stops: const [
+            0.1,
+            1,
+          ]),
+      borderGradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          const Color(0xFFffffff).withOpacity(0.5),
+          const Color((0xFFFFFFFF)).withOpacity(0.5),
         ],
       ),
-    ),
-  );
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Current Streak Start Date ${currentStreakStartDate.substring(0, 10)}",
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                // Padding(
+                //   padding: const EdgeInsets.all(8.0),
+                //   child: Text("Total Stars Earned : $totalStars"),
+                // ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Current Streak Length $currentStreakLength",
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Longest Streak Start Date ${longestStreakStartDate.substring(0, 10)}",
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Longest Streak End Date ${longestStreakEndDate.substring(0, 10)}",
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Longest Streak length $longestStreakLength",
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
